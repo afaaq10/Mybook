5d58ec8e57b7e31070ab05276038cda795f77988
Afaaq <afaaqmajeed123@gmail.com>
2022-03-25 18:47:56 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      outline: {
+        blue: '2px solid #0000ff',
+      }
+    },
+  },
+  plugins: [],
+}
